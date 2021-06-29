b2c5ad4eba6c0361a4c642a96246ae84f7951291
AhmadFel <i7mdia@hotmail.com>
2021-06-29 13:26:46 +0300

    SeederUpdate

--- database/seeders/BookingSeeder.php
+++ database/seeders/BookingSeeder.php
@@ -13,9 +13,9 @@ class BookingSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('booking')->insert(['room_id' => 1, 'customers_id' =>78,'from_date'=>'14','to_date'=>'15']);
-        DB::table('booking')->insert(['room_id' => 2, 'customers_id' =>59,'from_date'=>'16','to_date'=>'20']);
-        DB::table('booking')->insert(['room_id' => 3, 'customers_id' =>226,'from_date'=>'30','to_date'=>'5']);
-        DB::table('booking')->insert(['room_id' => 4, 'customers_id' =>44,'from_date'=>'25','to_date'=>'31']);
+        DB::table('booking')->insert(['room_id' => 1, 'customers_id' =>78,'from_date'=>'2020-07-1','to_date'=>'2020-07-20']);
+        DB::table('booking')->insert(['room_id' => 2, 'customers_id' =>59,'from_date'=>'2020-07-2','to_date'=>'2020-07-21']);
+        DB::table('booking')->insert(['room_id' => 3, 'customers_id' =>226,'from_date'=>'2020-07-3','to_date'=>'2020-07-22']);
+        DB::table('booking')->insert(['room_id' => 4, 'customers_id' =>44,'from_date'=>'2020-07-20','to_date'=>'2020-07-23']);
     }
 }
--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+
 class CustomerSeeder extends Seeder
 {
     /**
@@ -13,7 +14,11 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('customers')->insert(['name' => 'ahmad', 'mobile' =>03412,'email'=>'sss@hotmail.com']);
-        DB::table('customers')->insert(['name' => 'mohammed', 'mobile' =>78456,'email'=>'bbb@hotmail.com']);
+        DB::table('customers')->insert([
+            'name' => 'ahmad', 'mobile' => '05555507810', 'email' => 'sss@hotmail.com'
+        ]);
+        DB::table('customers')->insert([
+            'name' => 'mohammed', 'mobile' => '055589605', 'email' => 'bbb@hotmail.com'
+        ]);
     }
 }
